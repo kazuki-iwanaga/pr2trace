f0f97e5f4781fccbb1f3092de4b4dc6f0957c1c3
kazuki-iwanaga <47914321+kazuki-iwanaga@users.noreply.github.com>
2024-09-15 08:10:30 +0000

    comment out url flag

--- cmd/root.go
+++ cmd/root.go
@@ -32,7 +32,7 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.PersistentFlags().StringP("url", "u", "", "URL of the GitHub Pull Request")
+	// rootCmd.PersistentFlags().StringP("url", "u", "", "URL of the GitHub Pull Request")
 	rootCmd.PersistentFlags().StringP("owner", "o", "", "Owner of the GitHub repository")
 	rootCmd.PersistentFlags().StringP("repo", "r", "", "Name of the GitHub repository")
 	rootCmd.PersistentFlags().IntP("number", "n", 1, "Number of the GitHub Pull Request")
