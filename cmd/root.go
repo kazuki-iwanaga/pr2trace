7d2ab7a901a9f037172c749f48f881ac5c52d5ac
kazuki-iwanaga <47914321+kazuki-iwanaga@users.noreply.github.com>
2024-09-15 08:03:06 +0000

    update root

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -16,6 +17,11 @@ var rootCmd = &cobra.Command{
 	Long: `Convert GitHub Pull Request to OpenTelemetry-compatible telemetry.
 
 [TODO] A longer description and some examples will be written here.`,
+	Example: `  pr2otel --url https://github.com/kazuki-iwanaga/pr2otel/pull/7
+  pr2otel --owner kazuki-iwanaga --repo pr2otel --number 7`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hello World!")
+	},
 }
 
 func Execute() {
