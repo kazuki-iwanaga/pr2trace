f16b29fbfba87bf956fbb81cadc9fe5b662d7bcf
kazuki-iwanaga <47914321+kazuki-iwanaga@users.noreply.github.com>
2024-09-15 07:28:20 +0000

    add args and example

--- cmd/url.go
+++ cmd/url.go
@@ -7,13 +7,15 @@ import (
 )
 
 var urlCmd = &cobra.Command{
-	Use:   "url",
+	Use:   "url <url>",
 	Short: "Specify the Pull Request by URL",
 	Long: `Specify the Pull Request by URL
 
 [TODO] The details will be written here.`,
+	Args:    cobra.ExactArgs(1),
+	Example: "  pr2otel url https://github.com/kazuki-iwanaga/pr2otel/pull/7",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("url called")
+		fmt.Println("url called:", args[0])
 	},
 }
 
