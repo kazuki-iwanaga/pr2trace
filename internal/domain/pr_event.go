de56d876da83bb8170a10bf1f4c824eb8e11b538
kazuki-iwanaga <47914321+kazuki-iwanaga@users.noreply.github.com>
2024-10-06 05:50:28 +0000

    add prevent getter

--- internal/domain/pr_event.go
+++ internal/domain/pr_event.go
@@ -1,11 +1,32 @@
 package domain
 
+import "time"
+
 type (
 	PrEventType string
-	PrEvent     struct{}
+
+	PrEvent struct {
+		eventType PrEventType
+		createdAt time.Time
+	}
 )
 
 const (
 	PrEventOpened PrEventType = "Opened"
 	PrEventClosed PrEventType = "Closed"
 )
+
+func NewPrEvent(eventType PrEventType, createdAt time.Time) *PrEvent {
+	return &PrEvent{
+		eventType: eventType,
+		createdAt: createdAt,
+	}
+}
+
+func (e *PrEvent) EventType() PrEventType {
+	return e.eventType
+}
+
+func (e *PrEvent) CreatedAt() time.Time {
+	return e.createdAt
+}
